199e2b64d785b6873785a4990dd3e7790883f8a0
Marina Chukhnova <marina_chukhnova@ukr.net>
2019-09-21 19:55:11 +0300

    add error validate a password

--- main.js
+++ main.js
@@ -5,10 +5,8 @@ function nextSlide(parent,nextForm){
 	nextForm.classList.add('innactive');
 }
 
-
 function animatedForm() {
 	const arrows = document.querySelectorAll('.fa-arrow-right');	
-
 
 	arrows.forEach(arrow =>{
 		arrow.addEventListener('click', () =>{
@@ -25,13 +23,13 @@ function animatedForm() {
 				nextSlide(parent, nextForm);
 			}else if(input.type ==='email' && validateEmail(input)){
 				nextSlide(parent, nextForm);
-			}else if(input.type ==='password'&& validateUser (input)){
+			}else if(input.type ==='password'&& validatePassword(input)){
 				nextSlide(parent, nextForm);
 			}else{
-				parent.style.animation ='shake 0.5s ease ';
+				parent.style.animation ='shake 0.5s ease';
 			}
 			//get rid of animation
-			parent.addEventListener('animationend',()=>{
+			parent.addEventListener('animationend',() => {
 				parent.style.animation ='';
 			});
 		});	
@@ -48,9 +46,10 @@ function validateUser(user){
 		return true;
 	}
 }
+
 function validateEmail(email){
-	const validation =/^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-	if(validation.test(email.value)){
+	const validationEmail =/^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+	if(validationEmail.test(email.value)){
 		error('rgb(187,189,130)');
 		return true;
 	}else{
@@ -58,6 +57,35 @@ function validateEmail(email){
 	}
 }
 
+function validatePassword (password){
+	const validationPassword =/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})/;
+	const Error1 =/(?=.*[a-z])/;
+	const Error2 =/(?=.*[A-Z])/;
+	const Error3 =/(?=.*[0-9])/;
+	const Error4 =/(?=.*[!@#\$%\^&\*])/;
+	const Error5 =/(?=.{8,})/;
+
+	const errorText = document.querySelector('.error');
+	errorText.style.color = "white";
+	errorText.style.border = "2px solid white";
+
+	if(validationPassword.test(password.value)){
+		error('rgb(187,189,130)');
+		return true;
+	}else if (Error1.test(password.value) === false || 
+			  Error2.test(password.value) === false ||
+			  Error3.test(password.value) === false){
+		errorText.innerHTML = 'Your password is too simple.The password must contain at least 1 lowercase, uppercase and numeric alphabetical character.';
+		error('rgb(189,87,87)');
+	}else if (Error4.test(password.value) === false){
+		errorText.innerHTML = 'Change your password.These characters cannot be used to set a password!';
+		error('rgb(189,87,87)');
+	}else if (Error5.test(password.value) === false){
+		errorText.innerHTML = 'Change your password.The string must be eight characters or longer!';
+		error('rgb(189,87,87)');
+	}
+
+}
 
 function error(color){
 	document.body.style.backgroundColor = color;
